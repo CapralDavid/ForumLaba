1da5ecfe5f0c65c0a5d4c22cd98f275cb3e62b3e
CapralDavid <55207583+CapralDavid@users.noreply.github.com>
2021-03-04 17:00:07 +0200

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,23 @@
+
+function Laba(evt, Tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(Tab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
